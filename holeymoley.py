b8574ae894fcf921a3f4e729b0ce5bcea3613a78
mii-quail <paulomarku222@gmail.com>
2026-08-21 00:59:46 +0100

    Holy spaghetti code
    By far the coolest graphs are here but also the worst code...
    I cleaned any weird looking data (and trust me there was a lot of dirty data. Idk how it got there but it's dirty.)
    by using the 3DBrew Mii specification. The plots also have an image for guidance on where the mole might go on a real Mii.
                    x           y           s
    count  210.000000  210.000000  210.000000
    mean     6.842857  -18.842857    1.828571
    std      3.925024    5.381083    1.048646
    min      2.000000  -30.000000    0.000000
    25%      2.000000  -21.000000    1.000000
    50%     10.000000  -17.500000    2.000000
    75%     10.000000  -14.000000    3.000000
    max     11.000000  -12.000000    5.000000
                  x          y         s
    count  6.000000   6.000000  6.000000
    mean   7.666667 -22.333333  5.833333
    std    0.816497   7.711463  2.041241
    min    6.000000 -28.000000  4.000000
    25%    8.000000 -26.750000  4.000000
    50%    8.000000 -26.000000  5.500000
    75%    8.000000 -20.750000  7.750000
    max    8.000000  -8.000000  8.000000
    The EDA says this for the NNID and celebrity Miis. Just at a glance you can see the mean Manhattan distance is larger on the celebrity Miis, and to be fair only 6 celebrity Miis had valid moles, but still a noteworthy finding for later.
    This program plots where the moles go on the face and we can kinda see some patterns that emerge. I see some clusters here that look pretty interesting... but do those clusters actually exist, or is it all in my head?
    Using the elbow method, I found out wrongly that we need 3 clusters, and correctly that I needed 2! Stupid mistake but it was my first time deploying this method, so I'm not too angry at myself for it. I can see me forgiving myself in like a couple of months.
    However, there's a chance I should forgive myself a little sooner because I think I accidentally found findings that tell more of a story IMO.
    For the 2 clusters, the narrative seemed to be distance from the nose, which in my data usually meant closer to the top-right corner. A bit boring, but reliable. I was hoping to be able to differentiate between moles that were used as actual moles as opposed to moles used to "decorate" a Mii or accompany the other parts of its face.
    Using 3 clusters told me a lot more of a story. The clusters seemed to come in 3: the first one would be all the moles either side of the nose, which seems like a likely place for a mole to be. The second one included the moles around the eyes, which is a bit rarer and could be fair enough but could also be eye decoration - I know my celebrity Miis and as such I know one is Frida Kahlo, where the mole did in fact accompany the eye. The third cluster just seemed like a really weird place to have a mole, and I knew from my celebrity Miis most were to decorate the mouth... but I think some were just because it looked funny having it off the Mii. The moles move in 2D, after all, but the face is 3D.
    Pretty fun!

---
+++ holeymoley.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+# lots of variable definitions. mole count gives the count of moles, although I guess I don't need this
+# it's just there to make sure everything's working okay
+# zolecount gives the amount of data cleaned
+# the c in front of the names of arrays means it's for celebrity data
+molecount=[0,0]
+zolecount=[0,0]
+xpoints=[]
+cxpoints=[]
+ypoints=[]
+cypoints=[]
+sizes=[]
+csizes=[]
+# not perfect image but an approximation of where the mole goes. Remember Miis are 3D and
+# the image is 2D
+img=plt.imread("default.png")
+df=pd.read_csv(r"C:\Users\chees\OneDrive\Microsoft Teams Chat Files\Documents\world.csv")
+# collect the data, add it to the array as needed
+for i in range(1000):
+    toggle = df.iloc[i]["mole_enable"]
+    x,y = df.iloc[i]["mole_horizontal"], -df.iloc[i]["mole_vertical"]
+    size = df.iloc[i]["mole_size"]
+    if toggle and x<=16 and x>=0 and y>=-30 and y<=0 and size<=8 and size>=0:
+        molecount[0]+=1
+        xpoints+=[x]
+        ypoints+=[y]
+        sizes+=[size]
+    elif toggle:
+        zolecount[0]+=1
+        print(i)
+for i in range(1000,1050):
+    toggle = df.iloc[i]["mole_enable"]
+    x,y = df.iloc[i]["mole_horizontal"], -df.iloc[i]["mole_vertical"]
+    size = df.iloc[i]["mole_size"]
+    if toggle and x<=16 and x>=0 and y>=-30 and y<=0 and size<=8 and size>=0:
+        molecount[1]+=1
+        cxpoints+=[x]
+        cypoints+=[y]
+        csizes+=[size]
+    elif toggle:
+        zolecount[1]+=1
+# so we can do some EDA
+xpoints = np.array(xpoints)
+ypoints = np.array(ypoints)
+cxpoints = np.array(cxpoints)
+cypoints = np.array(cypoints)
+df2=pd.DataFrame({"x":xpoints,"y":ypoints,"s":sizes})
+df3=pd.DataFrame({"x":cxpoints,"y":cypoints,"s":csizes})
+print(df2.describe())
+print(df3.describe())
+# Kmeans stuff :D
+data = list(zip(np.concatenate([xpoints,cxpoints]),np.concatenate([ypoints,cypoints])))
+inertias = []
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i)
+    kmeans.fit(data)
+    inertias.append(kmeans.inertia_)
+# so we can plot with the sizes
+fig,ax=plt.subplots()
+ax.imshow(img,extent=[0,16,-30,0],aspect="auto")
+ax.scatter(xpoints,ypoints,marker="*",alpha=0.2,color="blue",label="freerange",s=np.array(sizes)*10)
+ax.scatter(cxpoints,cypoints,marker="*",alpha=0.2,color="red",label="celebrity",s=np.array(csizes)*10)
+plt.xlabel("Horizontal")
+plt.ylabel("Vertical")
+ax.legend()
+plt.xlim(0, 16)
+plt.ylim(-30, 0)
+plt.show()
+# the inertias of the data. used to find the elbow. I can just eyeball it luckily, so no need for silhouette
+plt.plot(range(1,11), inertias, marker="*")
+# knee point
+diffs=np.diff(inertias)
+second_diffs=np.diff(diffs)
+print(second_diffs)
+print(np.argmax(second_diffs)+2)
+plt.show()
+kmeans=KMeans(n_clusters=2, random_state=67) # the elbow is 2, but I made a mistake
+# and I thought it was 3. for that reason I accidentally started with 3, but it's lowkey more
+# interesting. I'll plot both.
+kmeans.fit(data)
+plt.figure()
+plt.imshow(img,extent=[0,16,-30,0],aspect="auto")
+plt.scatter(np.concatenate([xpoints,cxpoints]),np.concatenate([ypoints,cypoints]),marker="*",s=np.concatenate([sizes,csizes])*10,c=kmeans.labels_)
+plt.xlabel("Horizontal")
+plt.ylabel("Vertical")
+plt.xlim(0, 16)
+plt.ylim(-30, 0)
+plt.show()
+kmeans=KMeans(n_clusters=3, random_state=67) 
+kmeans.fit(data)
+plt.figure()
+plt.imshow(img,extent=[0,16,-30,0],aspect="auto")
+plt.scatter(np.concatenate([xpoints,cxpoints]),np.concatenate([ypoints,cypoints]),marker="*",s=np.concatenate([sizes,csizes])*10,c=kmeans.labels_)
+plt.xlabel("Horizontal")
+plt.ylabel("Vertical")
+plt.xlim(0, 16)
+plt.ylim(-30, 0)
+plt.show()
